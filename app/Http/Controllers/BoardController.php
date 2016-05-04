15a1989915519d448d2e1fa2f014d09f612670d9
Daniel <daniel.reguero@hotmail.com>
2016-05-04 19:09:48 -0400

    More variables to be passed to views,
    along with all the boards and all the threads related to the current
    board.

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Board;
+use App\Thread;
 use App\Http\Requests;
 
 class BoardController extends Controller
@@ -12,8 +13,9 @@ class BoardController extends Controller
         if(is_numeric($board_url)){
             return redirect(Board::where('id', $board_url)->first()->board_url);
         }
-        $board = Board::where('board_url', $board_url)->first();
-        $boards = Board::all();
-        return view('boards.show')->with('board', $board)->with('boards', $boards);
+        $board = Board::where('board_url', $board_url)->first(); //this board
+        $boards = Board::all(); //all boards
+        $threads = Thread::where('board_id', $board->id)->paginate(5); //all threads
+        return view('boards.show')->with('board', $board)->with('boards', $boards)->with('threads', $threads);
     }
 }
