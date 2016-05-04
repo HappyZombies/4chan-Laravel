751fdad38e397c45063244c4aa6b6743e9ad7a87
Daniel <daniel.reguero@hotmail.com>
2016-05-03 23:27:05 -0400

    JS to change banner image on refresh.

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,10 @@
 $(function(){
-    console.log("Test");
+    function RandomBanner(){
+        //I'm lazy
+        var images = new Array("1.gif", "2.gif", "3.gif", "4.gif", "5.png");
+        var imageNum = Math.floor(Math.random() * images.length);
+        var baseUrl = document.location.origin;
+        $('#bannerImg').attr('src', baseUrl+"/img/title/"+images[imageNum]);
+    }
+    window.onload = RandomBanner;
 });
