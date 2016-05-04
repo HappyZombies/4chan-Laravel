1a7a648750f9c15d880c94501d04bfddb6daa213
Daniel <daniel.reguero@hotmail.com>
2016-05-03 23:27:48 -0400

    Re arranged, and adjusted some layouts and views.

---
+++ resources/views/home/rules.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.home')
+
+@section('content')
+    <div class="container">
+
+        <h1>Hello I am Rules</h1>
+
+    </div>
+@endsection
--- resources/views/layout/home.blade.php
+++ resources/views/layout/home.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Laravel~!</title>
+    <title>4chan</title>
     <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
@@ -13,6 +13,22 @@
 <div class="container">
     @yield('content')
 
+    <!--Bottom Nav-->
+    <div class="col-md-10 col-md-offset-1">
+        <nav class="bottom-nav ">
+            <ul>
+                <li class="first"><a href="{{ URL::asset('/') }}">Home</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="{{ URL::asset('/') }}rules">Rules</a></li>
+                <li><a href="#">Support 4chan</a></li>
+                <li><a href="#">Advertise</a></li>
+                <li><a href="#">Press</a></li>
+                <li><a href="#">日本語</a></li>
+            </ul>
+        </nav>
+    </div>
 </div>
 </body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
