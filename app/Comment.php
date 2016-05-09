1e8b0ecea6cf3d39874e7e915c6c70b127704c3e
Daniel <daniel.reguero@hotmail.com>
2016-05-09 16:17:51 -0400

    Added fillables for comments

--- app/Comment.php
+++ app/Comment.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
+    protected $fillable = ['author', 'comment', 'file', 'thread_id'];
     public function threads(){
         $this->belongsTo(Thread::class);
     }
