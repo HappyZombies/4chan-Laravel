8b0a19d5af1b900d5f089408df81e6d72408eff2
Daniel <daniel.reguero@hotmail.com>
2016-05-08 23:13:22 -0400

    Added controllers to view threads and comments, and store new threads as well.

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -5,17 +5,28 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Board;
 use App\Thread;
+use App\Comment;
 use App\Http\Requests;
 
 class BoardController extends Controller
 {
-    public function show($board_url){
+    public function index($board_url){
         if(is_numeric($board_url)){
             return redirect(Board::where('id', $board_url)->first()->board_url);
         }
         $board = Board::where('board_url', $board_url)->first(); //this board
-        $boards = Board::all(); //all boards
-        $threads = Thread::where('board_id', $board->id)->paginate(5); //all threads
-        return view('boards.show')->with('board', $board)->with('boards', $boards)->with('threads', $threads);
+        $boards = Board::orderBy('board_url', 'asc')->paginate(50); //all boards, for menu
+        $threads = Thread::ofBoard($board->id)->orderBy('id', 'desc')->paginate(5); //all threads on this board.
+        $comments = new Comment();
+        return view('boards.index')->with('board', $board)->with('boards', $boards)->with('threads', $threads)->with('comments', $comments);
+    }
+    
+    public function show($board_url, $thread_id){
+        $board = Board::where('board_url', $board_url)->first();
+        $boards = Board::orderBy('board_url', 'asc')->paginate(50); //all boards, for menu
+        $matchThese = ['board_id' => $board->id, 'id'=>$thread_id];
+        $thread = Thread::where($matchThese)->first();
+        $comments = new Comment();
+        return view('boards.show')->with('board', $board)->with('boards', $boards)->with('thread', $thread)->with('comments', $comments);
     }
 }
--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -2,11 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Thread;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Input;
+use App\Board;
+use Illuminate\Support\Facades\Redirect;
 use App\Http\Requests;
 
 class ThreadController extends Controller
 {
-    //
+    public function store($board_id, Request $request){
+        $this->validate(request(), array(
+            'file' => 'required|image',
+            'title'=> 'max:30',
+            'author'=>'max:30',
+            'comment' => 'required|max:2000',
+        ));
+
+        $author = empty($request->input('author')) ? "Anonymous" : $request->input('author');
+        $extension = Input::file('file')->getClientOriginalExtension();
+        $fileName = uniqid().'.'.$extension;
+        $board = Board::findOrFail($board_id);
+        $comment =  preg_replace('#[\r\n]+#', "\n", $request->input('comment'));
+        $board->threads()->save(new Thread([
+            'author'=>$author,
+            'title'=>$request->input('title'),
+            'comment'=>$comment,
+            'file'=>$fileName,
+        ]));
+        Input::file('file')->move('uploads', $fileName);
+        return back(); //redirect to thread made.
+    }
 }
