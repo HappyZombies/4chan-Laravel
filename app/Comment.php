c702992a1ec0d653f4ea839fef3e09d4b761fd58
Daniel <daniel.reguero@hotmail.com>
2016-05-10 22:23:19 -0400

    Removed scopeOf, not needed anymore.

--- app/Comment.php
+++ app/Comment.php
@@ -10,7 +10,4 @@ class Comment extends Model
     public function threads(){
         $this->belongsTo(Thread::class);
     }
-    public function scopeOfThread($query, $thread_id){
-        return $query->where('thread_id', $thread_id);
-    }
 }
